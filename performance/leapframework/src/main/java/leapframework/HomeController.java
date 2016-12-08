6836e640c102f94b22583cf75cc515251584d0e1
Kael <penghj@bingosoft.net>
2016-12-08 10:59:30 +0800

    add leapframework

---
+++ performance/leapframework/src/main/java/leapframework/HomeController.java
@@ -0,0 +1,10 @@
+package leapframework;
+
+/**
+ * Created by kael on 2016/12/8.
+ */
+public class HomeController {
+    public String index(){
+        return "Hello World";
+    }
+}
---
+++ performance/leapframework/src/main/java/leapframework/MainTest.java
@@ -0,0 +1,12 @@
+package leapframework;
+
+import leap.webunit.server.TWebServer;
+
+/**
+ * Created by kael on 2016/12/8.
+ */
+public class MainTest {
+    public static void main(String[] args) {
+        TWebServer.main(null);
+    }
+}
